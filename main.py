e8e2aa711177e1098fe5dc4c7baf9c09fe7c8c5e
Shubham Sagar <67498601+codestar-712@users.noreply.github.com>
2021-10-30 12:31:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ from pyqrcode import QRCode
 Qrstring="https://codestarshub.blogspot.com/2020/12/generate-qr-code-using-python.html" #variable to store our link
 url=pyqrcode.create(Qrstring)#function to create the qr-code
 url.png('C:/Users/hp/Desktop/qr.png',scale=8)#create and save the png file at the location
+
+var=6
